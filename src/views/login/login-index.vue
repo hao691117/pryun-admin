70e35d6688c16469942b8b906785c78410949c8a
Breathe <704340916@qq.com>
2023-04-14 18:05:26 +0800

    a

--- src/views/login/login-index.vue
+++ src/views/login/login-index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="login">
+    <img class="login-bg" src="./static/bg.jpg" alt="" />
     <div class="login-content" @click="login">
       <div class="left">
         <!-- <img class="left-img" src="https://desk-fd.zol-img.com.cn/t_s960x600c5/g5/M00/09/0E/ChMkJ13Lc6uIGCPDAAF0o4kIEFcAAvNEgGM66oAAXS7641.jpg" alt="" /> -->
@@ -30,6 +31,11 @@ const login = () => {
   display: flex;
   align-items: center;
   justify-content: center;
+  .login-bg {
+    min-width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
   .login-content {
     width: calc(100% - 40px);
     max-width: 1600px;
@@ -40,7 +46,6 @@ const login = () => {
     // background-color: var(--color-bg);
     // border-radius: 12px;
     overflow: hidden;
-    box-shadow: 0 0 40px 10px rgba(64, 64, 64, 0.1);
     .left {
       position: relative;
       width: 0;
