9d1070e130d0cea75fd9e06937d6af5548036808
hao691117 <646038463@qq.com>
2023-07-18 17:34:14 +0800

    feat: 数据指标页面搭建

--- src/views/dataIndex/dataIndex-index.vue
+++ src/views/dataIndex/dataIndex-index.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>dataIndex</div>
+  <div class="reportAnalysis">
+    <div class="left">
+      <leftMenus></leftMenus>
+    </div>
+    <div class="right"></div>
+  </div>
 </template>
+<script lang="ts" setup>
+import leftMenus from './components/left-menus/left-menus.vue'
+</script>
+<style lang="scss" scoped>
+.reportAnalysis {
+  position: relative;
+  height: calc(100vh - 68px);
+  display: flex;
+  align-items: center;
+  .left {
+    width: 320px;
+    height: 100%;
+  }
+  .right {
+    width: 0;
+    height: 100%;
+    flex: 1;
+    border-left: 1px solid rgba(128, 128, 128, 0.2);
+    box-sizing: border-box;
+  }
+}
+</style>
