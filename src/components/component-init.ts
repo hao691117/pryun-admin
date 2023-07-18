50c99bf50c1e0c99b1da43e438c9080dfd522fea
zhaop <704340916@qq.com>
2023-07-18 10:40:03 +0800

    全局引入element-plus

--- src/components/component-init.ts
+++ src/components/component-init.ts
@@ -1,13 +1,12 @@
 import ContentTable from '@/components/ContentTable/ContentTable.vue'
 
+import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 export default {
   install: (app: any) => {
     app.component('ContentTable', ContentTable)
-    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-      app.component(key, component)
-    }
+    app.use(ElementPlus)
   },
 }
