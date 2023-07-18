bdff5fc09aa8f55b8c37ee524d5a5c2b22ea61ba
zhaop <704340916@qq.com>
2023-07-18 10:42:02 +0800

    el图标

--- src/components/component-init.ts
+++ src/components/component-init.ts
@@ -4,9 +4,15 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 export default {
   install: (app: any) => {
     app.component('ContentTable', ContentTable)
     app.use(ElementPlus)
+    // 注册全局的el-icon图标
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component)
+    }
   },
 }
